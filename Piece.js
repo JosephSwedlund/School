98d2e0e81a60fb566cbb326a550953d41fc78c7e
ddk16a <49002405+ddk16a@users.noreply.github.com>
2019-04-26 20:04:14 -0500

    Update Piece.js

--- Piece.js
+++ Piece.js
@@ -28,14 +28,3 @@ Piece.focus = function() {
 	Highlight.target = this;
 	highlight(this.pattern());
 }
-
-function Pawn(row, col, color) {
-	return Piece(row, col, color, Pawn.pattern);
-}
-
-Pawn.pattern = () => {
-	let piece = Highlight.target;
-	let tiles = new Array();
-	[0,1,2,3,5,7].forEach((dir) => tiles.push({ row: piece.row+Math.floor(dir/3)-1, col: piece.col+(dir%3)-1 }));
-	return tiles;
-};
