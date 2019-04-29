11f784f586cf13b7b75b4fcdfa58afee5ce2287a
ddk16a <49002405+ddk16a@users.noreply.github.com>
2019-04-29 14:14:49 -0500

    Update setup.js

--- setup.js
+++ setup.js
@@ -5,15 +5,15 @@ var player; //the color of the player
 
 $(function() {
 	player = "white";
-	stage = new Stage('myCanvas');
+	stage = new Stage('myCanvas'); //pass in the canvas id to set up easeljs
 
 	let ally = [
-		['Pawn','Pawn','Pawn','Pawn','Pawn','Pawn','Pawn','Pawn'],
-		['Pawn','Pawn','Pawn','Pawn','Pawn','Pawn','Pawn','Pawn']
+		['Pawn','Pawn','Pawn','Pawn','Pawn','Pawn','Pawn','Pawn'], //this is the bottom row
+		['Pawn','Pawn','Pawn','Pawn','Pawn','Pawn','Pawn','Pawn'] //this is one above the bottom row
 	];
 	let enemy = [
-		['Pawn','Pawn','Pawn','Pawn','Pawn','Pawn','Pawn','Pawn'],
-		['Pawn','Pawn','Pawn','Pawn','Pawn','Pawn','Pawn','Pawn']
+		['Pawn','Pawn','Pawn','Pawn','Pawn','Pawn','Pawn','Pawn'], //this is the top row
+		['Pawn','Pawn','Pawn','Pawn','Pawn','Pawn','Pawn','Pawn'] //this is one below the top row
 	];
 
 	stage.setupBoard(player, ally, enemy);
