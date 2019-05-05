0dba393370a5adebab30f3d92054a4448079bb6f
zcalbrecht <zcalbrecht@gmail.com>
2019-05-04 19:20:50 -0500

    Ensured the rook is part of the default gamestate

--- setup.js
+++ setup.js
@@ -8,11 +8,11 @@ $(function() {
 	stage = new Stage('myCanvas'); //pass in the canvas id to set up easeljs
 
 	let ally = [
-		['Pawn','Pawn','Pawn','Pawn','Pawn','Pawn','Pawn','Pawn'], //this is the bottom row
+		['Pawn','Rook','Pawn','Rook','Rook','Pawn','Rook','Pawn'], //this is the bottom row
 		['Pawn','Pawn','Pawn','Pawn','Pawn','Pawn','Pawn','Pawn'] //this is one above the bottom row
 	];
 	let enemy = [
-		['Pawn','Pawn','Pawn','Pawn','Pawn','Pawn','Pawn','Pawn'], //this is the top row
+		['Pawn','Rook','Pawn','Rook','Rook','Pawn','Rook','Pawn'], //this is the top row
 		['Pawn','Pawn','Pawn','Pawn','Pawn','Pawn','Pawn','Pawn'] //this is one below the top row
 	];
 
