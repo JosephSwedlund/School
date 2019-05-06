52bf501d378a80bac34efd30193d91c57900539d
ddk16a <49002405+ddk16a@users.noreply.github.com>
2019-05-06 15:27:08 -0500

    Add files via upload

--- js/Socket.js
+++ js/Socket.js
@@ -2,25 +2,15 @@ import Stage from './Stage.js';
 
 let socket = io();
 
-socket.on('connect', () => {
-	console.log('connected');
-});
-
-socket.on('setcolor', (color) => socket.color = color);
-
-socket.on('updateBoard', (piece, dest) => {
-	Stage.get(Stage.invert(piece.row), piece.col).moveTo(Stage.invert(dest.row), dest.col);
-	Stage.startTurn();
-	Stage.update();
-});
+socket.on('connect', () => console.log('connected'));
 
 var stage; 
 var player; //the color of the player
 
-socket.on('setup', () => {
+socket.on('pair', () => {
+	socket.emit('paired');
 	$(function() {
-		console.log(socket.color);
-		player = socket.color;
+		player = 'white';
 		stage = new Stage('myCanvas'); //pass in the canvas id to set up easeljs
 
 		let ally = [
@@ -36,6 +26,32 @@ socket.on('setup', () => {
 
 		stage.update();
 	});
+});
+
+socket.on('paired', () => {
+	$(function() {
+		player = 'black';
+		stage = new Stage('myCanvas'); //pass in the canvas id to set up easeljs
+
+		let ally = [
+			['Pawn','Rook','Pawn','Rook','Rook','Pawn','Rook','Pawn'], //this is the bottom row
+			['Pawn','Pawn','Pawn','Pawn','Pawn','Pawn','Pawn','Pawn'] //this is one above the bottom row
+		];
+		let enemy = [
+			['Pawn','Rook','Pawn','Rook','Rook','Pawn','Rook','Pawn'], //this is the top row
+			['Pawn','Pawn','Pawn','Pawn','Pawn','Pawn','Pawn','Pawn'] //this is one below the top row
+		];
+
+		stage.setupBoard(player, ally, enemy);
+
+		stage.update();
+	});
+});
+
+socket.on('updateBoard', (piece, dest) => {
+	Stage.get(Stage.invert(piece.row), piece.col).moveTo(Stage.invert(dest.row), dest.col);
+	Stage.startTurn();
+	Stage.update();
 });
 
 export default socket;
